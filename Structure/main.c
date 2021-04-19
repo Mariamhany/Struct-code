89c8511a528e58d7a05928084010297f256993cb
Mariamhany <mariam.hany159@gmail.com>
2021-04-20 01:40:38 +0200

    Structure code

---
+++ Structure/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct student {
+    char Name[50];
+    int ID;
+    int day, month, year;
+    int score;
+};
+int main()
+{
+    printf("Welcome to Student Information Program\n");
+    return 0;
+}
